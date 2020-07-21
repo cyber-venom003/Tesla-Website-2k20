ff339956f737b8047dca6d73237579066053d1d9
cyber-venom003 <tech.worm.1728@gmail.com>
2020-07-21 15:36:51 +0530

    Dynamic Components created. Refactoring remaining.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Coordinator from './components/coordinator';
+import Team from './components/team';
+import Inventory from './components/inventory';
 
-const jsx = <p>This is from React Component</p>;
+ReactDom.render(<Coordinator /> , document.getElementById("coordinators"));
 
-ReactDom.render(jsx , document.getElementById("react-id"))
+ReactDom.render(<Team /> , document.getElementById("team"));
+
+ReactDom.render(<Inventory /> , document.getElementById("inventory"));
